dfefa2c56801ceaf5ab075ded148ebe67f5ba095
Anton Drachylouski <anton.drachylouski@gmail.com>
2018-10-02 22:46:06 +0300

    step_2_finish

---
+++ KatlaSport.DataAccess/Migrations/201810021942003_AddProductDescriptionManCodePrice.cs
@@ -0,0 +1,16 @@
+namespace KatlaSport.DataAccess.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    /// <inheritdoc />
+    public partial class AddProductDescriptionManCodePrice : DbMigration
+    {
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
