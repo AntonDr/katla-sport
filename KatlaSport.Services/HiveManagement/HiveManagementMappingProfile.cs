7b6565bed5898634be842901a22bcc6f25eb791a
Anton Drachylouski <anton.drachylouski@gmail.com>
2018-10-05 02:10:52 +0300

    HivesController implements methods for adding, editing, and deleting Hive entities.

--- KatlaSport.Services/HiveManagement/HiveManagementMappingProfile.cs
+++ KatlaSport.Services/HiveManagement/HiveManagementMappingProfile.cs
@@ -1,9 +1,11 @@
-﻿using AutoMapper;
+﻿using System;
+using AutoMapper;
 using DataAccessHive = KatlaSport.DataAccess.ProductStoreHive.StoreHive;
 using DataAccessHiveSection = KatlaSport.DataAccess.ProductStoreHive.StoreHiveSection;
 
 namespace KatlaSport.Services.HiveManagement
 {
+    /// <inheritdoc />
     public sealed class HiveManagementMappingProfile : Profile
     {
         public HiveManagementMappingProfile()
@@ -12,6 +14,8 @@ namespace KatlaSport.Services.HiveManagement
             CreateMap<DataAccessHive, Hive>();
             CreateMap<DataAccessHiveSection, HiveSectionListItem>();
             CreateMap<DataAccessHiveSection, HiveSection>();
+            CreateMap<UpdateHiveRequest, DataAccessHive>()
+                .ForMember(r => r.LastUpdated, opt => opt.MapFrom(p => DateTime.UtcNow));
         }
     }
 }
